491e790b40bba4c8b50ecfc90b00a9f243e5cb28
Anantha Krishnan <ananthak803@gmail.com>
2025-05-07 21:06:50 +0530

    add ipynp

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 model = tf.keras.models.load_model('.\pneumonia_detection_model.h5')
-img_path = './test/p1.jpeg' 
+img_path = './test/n1 (3).jpeg' 
 img = cv2.imread(img_path)
 img = cv2.resize(img, (150, 150))  
 img = np.expand_dims(img, axis=0)  
